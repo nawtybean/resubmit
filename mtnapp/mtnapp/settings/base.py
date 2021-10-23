b58cbd6276d1011b99e62a690575ed1dfe6cfb30
nawtybean <shaun@deponte.co.za>
2021-10-23 16:25:07 +0200

    static

--- mtnapp/mtnapp/settings/base.py
+++ mtnapp/mtnapp/settings/base.py
@@ -75,9 +75,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
