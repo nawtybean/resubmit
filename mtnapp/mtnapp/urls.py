ed86ddf3a0c4c86e499991dae80cef33c2169476
nawtybean <shaun@deponte.co.za>
2021-10-23 16:31:10 +0200

    static

--- mtnapp/mtnapp/urls.py
+++ mtnapp/mtnapp/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('akmtnsubmit.urls'))
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
